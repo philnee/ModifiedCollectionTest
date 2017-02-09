fe21b0448f5fa18c6edad74cd829ee5abaa32443
Phil <philneedham85@gmail.com>
2017-02-09 20:17:43 +0000

    Using syncronised collection

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,26 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
-        private static readonly object LockObject = new object();
-        readonly List<int> _list = new List<int>();
+        readonly SynchronizedCollection<int> _list = new SynchronizedCollection<int>();
         public void Run()
         {
             for (int i = 0; i < 1000; i++)
             {
                 Task.Run(() =>
                 {
-                    lock (LockObject)
+                    _list.Add(i);
+                    if (_list.Count >= 50)
                     {
-                        _list.Add(i);
-                        if (_list.Count >= 50)
-                        {
-                            Class2.DoThings(_list);
-                            _list.Clear();
-                        }
+                        Class2.DoThings(_list.ToList());
+                        _list.Clear();
                     }
                 });
             }
